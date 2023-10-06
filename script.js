171a8fde88806bb648f524934ee2ab263f1f12b9
GloDy KALOMBO <glodykalombo123@gmail.com>
2023-10-06 23:51:19 +0900

    creation tableaux

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-console.log('Bonjour')
+const texts = ['Bonjour', 'Faim', 'Travail', 'Nuit'];
+const messages = ["On se lève, c'est déjà le matin", "c'est l'heure de manger, la nourriture est prète", "c'est l'heure du travail", "il faut dormir", "Merci !"];
